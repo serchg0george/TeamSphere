dd9d5515e8ba04123f4f21c4755ac45db1ae9921
Serhii Zavada <serchg0george@gmail.com>
2025-03-20 18:25:40 +0200

    refactor: expanded TaskInfo dto with other parameters
    fixed mapper accordingly to new dto

--- TeamSphere-backend/src/main/java/com/teamsphere/dto/employee/TaskInfo.java
+++ TeamSphere-backend/src/main/java/com/teamsphere/dto/employee/TaskInfo.java
@@ -1,4 +1,4 @@
 package com.teamsphere.dto.employee;
 
-public record TaskInfo(Long id, String taskNumber, String taskStatus) {
+public record TaskInfo(Long id, String taskNumber, String taskStatus, String taskDescription, Integer timeSpentMinutes) {
 }
--- TeamSphere-backend/src/main/java/com/teamsphere/mapper/EmployeeMapper.java
+++ TeamSphere-backend/src/main/java/com/teamsphere/mapper/EmployeeMapper.java
@@ -43,7 +43,7 @@ public class EmployeeMapper implements BaseMapper<EmployeeEntity, EmployeeDto> {
                 .departmentName(entity.getDepartment().getDepartmentName())
                 .positionName(entity.getPosition().getPositionName())
                 .tasks(entity.getTasks().stream()
-                        .map(task -> new TaskInfo(task.getId(), task.getTaskNumber(), task.getTaskStatus().toString()))
+                        .map(task -> new TaskInfo(task.getId(), task.getTaskNumber(), task.getTaskStatus().toString(), task.getTaskDescription(), task.getTimeSpentMinutes()))
                         .toList())
                 .projects(entity.getProjects().stream()
                         .map(project -> new ProjectInfo(project.getId(), project.getName()))
